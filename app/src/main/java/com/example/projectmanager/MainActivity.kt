88871e5e7f6a1b83d4057c829eb9aa2470c3be48
Fredrik Karlsson <kafr18ga@student.ju.se>
2020-02-10 19:41:26 +0100

    feat: Added authentication and session to the activities

--- app/src/main/java/com/example/projectmanager/MainActivity.kt
+++ app/src/main/java/com/example/projectmanager/MainActivity.kt
@@ -4,6 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.projectmanager.Auth.LoginActivity
+import com.example.projectmanager.Managers.AuthenticationManager
+import com.example.projectmanager.Managers.SessionManager
+import com.example.projectmanager.Utilites.FirebaseAuthentication
+import com.example.projectmanager.Utilites.FirebaseSession
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +15,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // DENNA SKA FLYTTAS https://stackoverflow.com/questions/7360846/how-can-i-execute-something-just-once-per-application-start
+        AuthenticationManager.init(FirebaseAuthentication())
+        SessionManager.init(FirebaseSession())
 
         button.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
---
+++ app/src/main/java/com/example/projectmanager/Utilites/CurrentUser.kt
@@ -0,0 +1,7 @@
+package com.example.projectmanager.Utilites
+
+data class CurrentUser (
+    var uid: String,
+    var displayName: String,
+    var email: String
+)
