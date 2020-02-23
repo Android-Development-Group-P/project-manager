d540972b02680f686d62dcb30c7f1cd987ac29e5
Fredrik Karlsson <kafr18ga@student.ju.se>
2020-02-23 13:39:03 +0100

    feat: Added a "QR Code" generator

---
+++ app/src/main/java/com/example/projectmanager/util/QRGenerator.kt
@@ -0,0 +1,32 @@
+package com.example.projectmanager.util
+
+import android.graphics.Bitmap
+import android.graphics.Color
+import com.google.gson.annotations.SerializedName
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.qrcode.QRCodeWriter
+import java.io.Serializable
+
+object QRGenerator {
+
+    private val writer = QRCodeWriter()
+
+    fun generate(qr: QRObject) : Bitmap {
+        val matrix = writer.encode(qr.content, BarcodeFormat.QR_CODE, qr.width!!, qr.height!!)
+        val bitmap = Bitmap.createBitmap(qr.width!!, qr.height!!, Bitmap.Config.RGB_565)
+
+        for (x in 0 until qr.width!!) {
+            for (y in 0 until qr.height!!) {
+                bitmap.setPixel(x, y, if (matrix.get(x, y)) Color.BLACK else Color.WHITE)
+            }
+        }
+
+        return bitmap
+    }
+
+    data class QRObject (
+        var content: String? = null,
+        var width: Int? = null,
+        var height: Int? = null
+    ) : Serializable
+}
